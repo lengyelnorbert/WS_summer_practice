54a29fbe33db77f58e6b3507b73f5146b6e7eefa
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-08-02 10:47:28 +0200

    stack exercise

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
+import stack.Stack;
 
 public class Main {
 
   public static void main(String[] args) {
 
+    Stack newStack = new Stack();
+    System.out.println(newStack.top());
+    newStack.push(8);
+    System.out.println(newStack.top());
+    newStack.push(9);
+    System.out.println(newStack.top());
+    newStack.push(22);
+    System.out.println(newStack.top());
+    newStack.pop();
+    newStack.push(222);
+    System.out.println(newStack.toString());
+    newStack.pop();
+    newStack.pop();
+    newStack.pop();
+    newStack.pop();
 
   }
-
 }
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,22 +1,56 @@
 package stack;
 
-import java.util.List;
+
+import java.util.Arrays;
 
 public class Stack {
 
-  private int[] stack = new int[10];
+  private int[] stack = new int[3];
   private int top = -1;
   private int tail = -1;
 
+  public Stack() {
+  }
 
-  private void push (int x){
-    if (top == -1){
-      System.out.println("empty stack");
-      top ++;
+  public void push(int x) {
+    if (isEmpty()) {
+      top++;
       stack[top] = x;
-      else if (top + 1 == tail){
-        System.out.println("");
-      }
+    } else if (top + 1 == stack.length) {
+      System.out.println("stack is full");
+    } else {
+      top++;
+      stack[top] = x;
     }
   }
+
+  public int top() {
+    if (isEmpty()) {
+      System.out.println("the stack is empty");
+      return -1;
+    }
+    return stack[top];
+  }
+
+  public void pop() {
+    if (isEmpty()) {
+      System.out.println("empty stack, nothing to pop");
+    } else {
+      top--;
+    }
+  }
+
+  public boolean isEmpty(){
+    return (top == -1);
+  }
+
+  @Override
+  public String toString() {
+    return "Stack{" +
+            "stack=" + Arrays.toString(stack) +
+            ", top=" + top +
+            ", tail=" + tail +
+            '}';
+  }
 }
+
